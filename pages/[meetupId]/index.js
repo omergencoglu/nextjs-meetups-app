6530bd7037e3aea8e580d95c09a10dd7203e94d4
Omer Gencoglu <omersaitgencoglu@gmail.com>
2022-02-17 12:52:12 -0500

    Add MeetupDetail component

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      title="Firts Meetup"
+      address="Some address"
+      description="Description"
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+    />
+  );
+}
+
+export default MeetupDetails;
